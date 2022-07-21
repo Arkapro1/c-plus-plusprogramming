ce0b35276517089bcc9a66b29efb1a10f83d68b2
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-07-21 03:51:26 +0000

    done oh

---
+++ stack-queue/stack/stlStack.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+  cout<<"yo";
+stack<int>s;
+cout<<"enter how many elements do u want to push: "<<endl;
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+    int x;
+    cin>>x;
+    s.push(x);
+}
+cout<<"printing your stack and poping"<<endl;
+while(!s.empty())
+{cout<<s.top()<<endl;
+s.pop();}
+cout<<"stack is empty"<<endl;
+
+}
