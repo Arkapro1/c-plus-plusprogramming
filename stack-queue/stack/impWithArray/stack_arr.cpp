5a547997902bbf95deba059c35631a61fd1aa50c
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-07-25 08:46:21 +0000

    satck wuth array done

--- stack-queue/stack/impWithArray/stack_arr.cpp
+++ stack-queue/stack/impWithArray/stack_arr.cpp
@@ -2,39 +2,54 @@
 using namespace std;
 class stacks{
 public:
-int capacity;
-int top=-1;
+int size;
+int top;
+int* arr;
 stacks (int size){
-    int arr[size];
-    capacity=size;
+    arr=new int[size];
+    this->size=size;
+    top=-1;
     cout<<"done"<<endl;
 }
 void push(int data){
-    if(isFull){ cout<<"stack is full 🙇‍♀️"<<endl; return;}
+    if(isFull()){ cout<<"stack is full 🙇‍♀️"<<endl; return;}
     top++;
     arr[top]=data;
 }
 void pop(){
-    if(isEmpty){ cout<<"stack is empty 🙇‍♀️"<<endl; return;}
-    arr[top]=NULL;
+    if(isEmpty()){ cout<<"stack is empty 🙇‍♀️"<<endl; return;}
+    // arr[top]=NULL;
     top--;
 }
-int top(){
-     if(isEmpty){ cout<<"stack is empty 🙇‍♀️"<<endl; return;}
+int topPeek(){
+     if(isEmpty()){ cout<<"stack is empty 🙇‍♀️"<<endl; return -1;}
      return arr[top];
 }
 bool isFull(){
-if(top==capacity-1) return 1;
-return 0;
+if(top==size-1) return true;
+return false;
 }
 bool isEmpty(){
-if(top==-1) return 1;
-return 0;
+if(top==-1) return true;
+return false;
 }
 };
 
 
 int main(){
 stacks s(5);
-if(s.isEmpty)cout<<"fine"<<endl;
+if(s.isEmpty())cout<<"fine the stack is empty at the inertial stage 🐱‍🏍"<<endl;
+s.push(1);
+s.push(2);
+s.push(3);
+s.push(4);
+s.push(5);
+s.push(6);
+cout<<"so u r not able to oush any more "<<endl;
+while(!s.isEmpty()){
+cout<<"the s.topPeek is: "<<s.topPeek()<<endl;
+s.pop();
+}
+cout<<"after popping all"<<endl;
+if(s.isEmpty())cout<<"stack is 🀄 empty"<<endl;
 }
---
+++ stack-queue/stack/impWithArray/tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+ push(int data){
+//     if(isFull){ cout<<"stack is full 🙇‍♀️"<<endl; return;}
+//     top++;
+//     arr[top]=data;
+// }
+// void pop(){
+//     if(isEmpty){ cout<<"stack is empty 🙇‍♀️"<<endl; return;}
+//     arr[top]=NULL;
+//     top--;
+// }
+// int top(){
+//      if(isEmpty){ cout<<"stack is empty 🙇‍♀️"<<endl; return;}
+//      return arr[top];
+// }
+// bool isFull(){
+// if(top==capacity-1) return true;
+// return false;
+// }
