b5f7e247db2a6c79c39d6dc3de68d0f5f3727860
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-08-02 20:59:48 +0000

    namespace started

---
+++ Epic explore/namespace.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+namespace std{
+     int gar=3;
+}
+// using namespace chumu;
+int main(){
+    // int gar=9;
+// cout<<chumu::gar<<endl;
+cout<<gar;
+
+
+}
