7f728f702e109aa7d4472339eee9461d0e6ac639
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-09-07 14:38:49 +0000

    ok fov coming

---
+++ Epic explore with OOps/namespace./namespaceAdv.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+namespace ankan{
+     class cl{
+        public:
+    void ptr();
+};
+void ankan::cl::ptr(){
+    cout<<"hellow ankan"<<endl;
+}
+}
+// using namespace chumu;
+int main(){
+ankan::cl obj;
+obj.ptr();
+// cout<<gar<<endl;
+}
---
+++ Epic explore with OOps/namespace./namespaceBasic.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+namespace mtCustmPrint{
+   string printhl="helow bro";
+}
+// using namespace chumu;
+int main(){
+cout<<mtCustmPrint::printhl<<endl;
+}
