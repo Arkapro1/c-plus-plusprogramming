30447f64b0c3b28408427e9de65c96d09f583051
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2022-09-07 20:12:23 +0530

    Delete Epic explore directory

--- Epic explore/namespace./namespaceBasic.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-namespace mtCustmPrint{
-   string printhl="helow bro";
-}
-// using namespace chumu;
-int main(){
-cout<<mtCustmPrint::printhl<<endl;
-}
